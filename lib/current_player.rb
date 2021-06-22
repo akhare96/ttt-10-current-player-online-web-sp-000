c23b282c2ecae6a2f73c75688782f792827b741c
Ambar Khare <ambar.khare24@gmail.com>
2021-06-22 16:21:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,10 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
-  turn_count = 0
+  turn = 0
   board.each do |count|
     if count == "X" || count == "O"
-      turn_count += 1
+      turn += 1
       return count
     end
   end
